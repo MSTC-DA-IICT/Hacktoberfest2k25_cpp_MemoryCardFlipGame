3734f16db74996faaa23583cadfaddffbd9c6380
Dev-KavyT <thkrjanak@gmail.com>
2025-11-08 00:01:39 +0530

    Initial commit - pushing all local contents

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/junaid29707/Desktop/202401102/Hacktoberfest/new/NewNew/Hacktoberfest2k25_cpp_MemoryCardFlipGame
-# Build directory: /home/junaid29707/Desktop/202401102/Hacktoberfest/new/NewNew/Hacktoberfest2k25_cpp_MemoryCardFlipGame/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(memory_card_game_tests "/home/junaid29707/Desktop/202401102/Hacktoberfest/new/NewNew/Hacktoberfest2k25_cpp_MemoryCardFlipGame/build/tests/memory_card_game_tests")
-set_tests_properties(memory_card_game_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/junaid29707/Desktop/202401102/Hacktoberfest/new/NewNew/Hacktoberfest2k25_cpp_MemoryCardFlipGame/CMakeLists.txt;137;add_test;/home/junaid29707/Desktop/202401102/Hacktoberfest/new/NewNew/Hacktoberfest2k25_cpp_MemoryCardFlipGame/CMakeLists.txt;0;")
-subdirs("_deps/raylib-build")
